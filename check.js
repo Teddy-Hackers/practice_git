9f08ceb33226e759c6b2d36917561224984d227a
Dmitry Kurtaev <dmitry.kurtaev+github@gmail.com>
2020-08-05 20:17:28 +0300

    test commit for task status check

---
+++ check.js
@@ -0,0 +1,18 @@
+const lib = require('common');
+
+module.exports.check = function check(username, token, callback) {
+  lib.github_api_get('https://api.github.com/repos/' + username + '/practice_git', token, (repo) => {
+    console.log(repo);
+    callback([{
+      "name": "0_repository",
+      "html_url": "",
+      "status": "success"
+    }]);
+  }, (err) => {
+    callback([{
+      "name": "0_repository",
+      "html_url": "",
+      "status": "neutral"
+    }]);
+  });
+}
