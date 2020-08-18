3b0e1bad9f0e3e43322d8920516e47cef993de71
Dmitry Kurtaev <dmitry.kurtaev+github@gmail.com>
2020-08-18 22:09:31 +0300

    Add checkAll method to return admin view

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const lib = require('common');
 
-module.exports.check = function check(username, token, callback) {
+module.exports.check = function(username, token, callback) {
   var results = [];
   var url = 'https://api.github.com/repos/' + username + '/practice_git';
   var test_branch_name = 'my_new_branch';
@@ -12,13 +12,14 @@ module.exports.check = function check(username, token, callback) {
     results.push({
       'name': name,
       'description': 'docs/' + name + '.md',
-      'html_url': 'https://github.com/Teddy-Hackers/practice_git/blob/master/docs/' + name + '.md',
+      'html_url': undefined,
       'status': 'neutral'
     });
 
-    test((passed) => {
+    test((passed, ref) => {
       if (passed) {
         results[id].status = 'success';
+        results[id].html_url = ref;
       }
       num += 1;
       if (num == num_tasks) {
@@ -32,7 +33,7 @@ module.exports.check = function check(username, token, callback) {
   add_task('repository', function(passed) {
     // Check that fork exists
     lib.github_api_get(url, token, () => {
-      passed(true);
+      passed(true, 'https://github.com/' + username + '/practice_git');
     }, (err) => { passed(false); });
   });
 
@@ -48,7 +49,7 @@ module.exports.check = function check(username, token, callback) {
           break;
         }
       }
-      passed(is_created);
+      passed(is_created, 'https://github.com/' + username + '/practice_git/tree/' + test_branch_name);
     }, (err) => { passed(false); });
   });
 
@@ -57,7 +58,11 @@ module.exports.check = function check(username, token, callback) {
   add_task('commit', function(passed) {
     // Check pushed commit
     lib.github_api_get(url + '/commits/refs/heads/' + test_branch_name, token, (data) => {
-      passed(data.author.login.localeCompare(username) == 0);
+      if (data.author.login.localeCompare(username) == 0) {
+        passed(true, data.html_url);
+      } else {
+        passed(false);
+      }
     }, (err) => { passed(false); });
   });
 
@@ -66,6 +71,7 @@ module.exports.check = function check(username, token, callback) {
   add_task('pull_request', function(passed) {
     lib.github_api_get('https://api.github.com/repos/Teddy-Hackers/practice_git/pulls?state=closed', token, (pulls) => {
       var is_merged = false;
+      var ref = undefined;
       for (var i = 0; i < pulls.length; ++i) {
         if (pulls[i].merged_at && pulls[i].user.login.localeCompare(username) == 0) {
           is_merged = true;
@@ -81,4 +87,25 @@ module.exports.check = function check(username, token, callback) {
   add_task('aliases', function(passed) {
     passed(false);
   });
+}
+
+module.exports.checkAll = function(token, callback) {
+  let repos = {};
+  lib.github_api_get('https://api.github.com/repos/Teddy-Hackers/practice_git/forks', token, (forks) => {
+    if (forks.length == 0) {
+      callback(repos);
+      return;
+    }
+
+    var num = 0;
+    forks.forEach((fork) => {
+      module.exports.check(fork.owner.login, token, (tasks) => {
+        repos[fork.owner.login] = tasks;
+        num += 1;
+        if (num == forks.length) {
+          callback(repos);
+        }
+      });
+    });
+  }, (err) => {console.log(err)});
 }
